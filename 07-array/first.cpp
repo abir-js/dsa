2928750a17dde0341915fd2469e1994e2ebd7dac
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-06-01 12:27:37 +0530

    Add initial implementation of array output in first.cpp

---
+++ 07-array/first.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a[5] = {18, 13, 35, 12, 50};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
