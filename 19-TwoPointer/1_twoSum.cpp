d09efc4fe5b8bf588dd7991a74c4b000429f1a33
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-07-27 02:37:28 +0530

    Add implementation for Two Sum problem

---
+++ 19-TwoPointer/1_twoSum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+	//code here
+		vector<int> ans;
+		int n = nums.size();
+		int start = 0, end = n-1;
+		while(start <= end){
+			if(nums[start]+nums[end] == target){	
+				ans.push_back(start+1);		
+				ans.push_back(end+1);	
+				return ans;	
+			} else if(nums[start] + nums[end] < target){	
+				start++;	
+			} else {	
+				end--;	
+			}
+		}
+		return {};
+	
+	}
+
+};
