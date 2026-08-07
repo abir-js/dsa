0603f34ce49d87975c0dc65cbaeebcc2c0af5792
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-08-07 20:45:20 +0530

    Add initial implementation for string operations in first.cpp

---
+++ 24-strings/first.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+
+    // string s1;
+
+    // cin >> s1;
+
+    // cout << s1;
+
+    // String operations
+
+    char c = 'a' + 2;
+    cout << c;
+
+    char a[5] = {'a', 'b', 'c', 'd', 'e'};
+    cout << a[0] << a[1] << a[2] << a[3] << a[4];
+
+    char b[5] = "abcd";
+    cout << b[0] << b[1] << b[2] << b[3] << b[4];
+
+
+    return 0;
+}
