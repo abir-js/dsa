6f27c50d45ee1af2b9da41bd49370b8b9bc8ac5c
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-08-26 21:55:30 +0530

    Add initial implementation of pointer demonstration in first.cpp

---
+++ 30-pointer-p2/first.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int *p = new int[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> p[i];
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << p[i] << " ";
+    }
+
+    return 0;
+}
