d5db8bd4799d5b66bb03f7d2049fff387f789904
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-08-27 09:12:53 +0530

    sum of n numbers using recursion

---
+++ 31-recursion/sum-of-n-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int sumOfN(int n)
+{
+    if (n == 0)
+        return 0;
+
+    return n + sumOfN(n - 1);
+}
+
+int main()
+{
+    cout << sumOfN(10) << endl;
+    return 0;
+}
